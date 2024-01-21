37befa07202c41ab53c14b6ea8c7d2f5e041fbe9
Palvi Dhawan <palvidhawan@Palvis-MacBook-Air.local>
2024-01-22 01:07:51 +0530

    Add login btn under header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { LOGO_URL } from "../utils/constants.js";
 
 const Header = () => {
+  const [loginBtn, setLoginBtn] = useState("Login")
     return (
       <div className="header">
         <div className="logo-container">
@@ -16,6 +17,11 @@ const Header = () => {
             <li>About</li>
             <li>Contact</li>
             <li>Cart</li>
+            <button className="login-btn" onClick={() => {
+              loginBtn === 'Login'
+              ? setLoginBtn("Logout")
+              : setLoginBtn("Login")
+            }}>{loginBtn}</button>
           </ul>
         </div>
       </div>
