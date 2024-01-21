2d5e8f673b4da728b020d597dceeb24e7c124b78
Palvi Dhawan <palvidhawan@Palvis-MacBook-Air.local>
2024-01-22 00:11:41 +0530

    fetch resturant data from api and add shimmer component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Resturant from "./Resturant";
+import Shimmer from "./Shimmer";
 import {restaurants} from "../utils/mockData";
 
 const Body = () => {
-    const [resList, setResList] = useState(restaurants);
+    const [resList, setResList] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    },[]);
+
+    const fetchData = async() => {
+        const data  = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+       const json = await data.json();
+       const newData = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+       setResList(newData);
+
+    }
     const getTopRatedResturants = () => {
         let getTopRatedResturantList = resList.filter((res) =>  res.info.avgRating > 4.2);
         setResList(getTopRatedResturantList);
+    }
+    if(resList.length === 0){
+        return <Shimmer/>
     }
     return (
       <div className="body-container">
